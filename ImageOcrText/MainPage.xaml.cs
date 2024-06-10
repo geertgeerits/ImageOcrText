7abde37d38336db90141841fb1e16c672b252c27
Geert Geerits <ggeerits@hotmail.com>
2024-06-10 08:56:25 +0200

    Update

--- ImageOcrText/MainPage.xaml.cs
+++ ImageOcrText/MainPage.xaml.cs
@@ -2,7 +2,7 @@
  * Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
  * Copyright ...: (C) 2024-2024
  * Version .....: 1.0.4
- * Date ........: 2024-06-09 (YYYY-MM-DD)
+ * Date ........: 2024-06-10 (YYYY-MM-DD)
  * Language ....: Microsoft Visual Studio 2022: .NET MAUI 8 - C# 12.0
  * Description .: Convert text from an image or picture to raw text via OCR
  * Note ........: 
