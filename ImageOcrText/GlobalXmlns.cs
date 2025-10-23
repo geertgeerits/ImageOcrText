a0e46e56587bb2bff8f77138136e696b7a975bd6
Geert Geerits <ggeerits@hotmail.com>
2025-10-23 12:32:58 +0200

    Adding GlobalXmlns.cs

--- ImageOcrText/GlobalXmlns.cs
+++ ImageOcrText/GlobalXmlns.cs
@@ -1,1 +1,3 @@
 ﻿[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ImageOcrText")]
+[assembly: XmlnsDefinition("http://imageocrtext/schemas/global", "ImageOcrText")]
+//[assembly: XmlnsDefinition("http://imageocrtext/schemas/resx", "ImageOcrText.Resources.Languages")]
