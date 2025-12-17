bebf632f75eed58f5d983a68ec34f541b6eea85a
Geert Geerits <ggeerits@hotmail.com>
2025-12-17 08:48:21 +0100

    Update NuGet package

--- ImageOcrText/MainPage.xaml.cs
+++ ImageOcrText/MainPage.xaml.cs
@@ -2,7 +2,7 @@
  * Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
  * Copyright ...: (C) 2024-2026
  * Version .....: 1.0.11
- * Date ........: 2025-12-12 (YYYY-MM-DD)
+ * Date ........: 2025-12-17 (YYYY-MM-DD)
  * Language ....: Microsoft Visual Studio 2026: .NET MAUI 10 - C# 14.0
  * Description .: Convert text from an image or picture to raw text via OCR
  * Note ........: Only portrait mode is supported for iOS (!!!BUG!!! problems with the editor in iOS when turning from landscape to portrait)
