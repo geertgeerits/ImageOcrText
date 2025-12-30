231c5af75e4d8a006bedb59524f8fd21e300d9ba
Geert Geerits <ggeerits@hotmail.com>
2025-12-30 14:43:59 +0100

    Update to version 1.0.12

--- ImageOcrText/PageInfo.xaml.cs
+++ ImageOcrText/PageInfo.xaml.cs
@@ -18,7 +18,7 @@ namespace ImageOcrText
             lblTitlePage.Margin = new Thickness(70, 5, 0, 0);
 #endif
             //// Put text in the chosen language in the controls and variables
-            lblVersion.Text = $"{OcrLang.Version_Text} 1.0.11";
+            lblVersion.Text = $"{OcrLang.Version_Text} 1.0.12";
             lblCopyright.Text = $"{OcrLang.Copyright_Text} © 2024-2026 Geert Geerits";
             lblPrivacyPolicy.Text = $"\n{OcrLang.PrivacyPolicyTitle_Text} {OcrLang.PrivacyPolicy_Text}";
             lblLicense.Text = $"\n{OcrLang.LicenseTitle_Text}: {OcrLang.License_Text}";
